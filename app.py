3ecbf3d801e85e3ddbaed2a47b4a791a67878916
nhlinh2207 <90165371+nhlinh2207@users.noreply.github.com>
2023-01-06 14:54:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,69 +10,69 @@ st.set_page_config(
 )
 
 diseases = {
-    'pink_eye': 'Pink Eye',
-    'diarrhea': 'Diarrhea',
-    'otitis_externa': 'Otitus Externa',
-    'skin_rash': 'Skin Rash',
-    'urinary_tract_infection': 'Urinary Tract Infection'
+    'pink_eye': 'Đau mắt đỏ',
+    'diarrhea': 'Tiêu chảy',
+    'otitis_externa': 'Viêm tai ngoài',
+    'skin_rash': 'Da phát ban',
+    'urinary_tract_infection': 'Nhiễm trùng đường tiểu'
 }
 
 with st.form('symtom_form'):
     st.markdown('# Symptoms')
     with st.expander(label='General', expanded=True):
-        gender = st.radio(label='gender', index=1,
+        gender = st.radio(label='Giới tính', index=1,
                           options=['Male', 'Female'], horizontal=True)
         gender_encoded = 0 if gender == 'Female' else 1
-        age = st.slider(label='age', min_value=5,
+        age = st.slider(label='Tuổi', min_value=5,
                         max_value=50, value=20, step=1)
-        swimming_time = st.number_input(
-            label="swimming_time", min_value=0, max_value=24)
+        swimming_time = st.slider(label='Thời gian bơi', min_value=1,
+                        max_value=24, value=1, step=1)
         swimming_time = swimming_time * 3600
-        fever = st.radio(label='fever', index=1,
+        fever = st.radio(label='Sốt', index=1,
                                options=['Yes', 'No'], horizontal=True)
     cols1 = st.columns(2)
     with cols1[0]:
         with st.expander(label='Eye', expanded=True):
-            conjunctiva_swelling = st.radio(label='conjunctiva_swelling', index=1,
+            conjunctiva_swelling = st.radio(label='Sưng giác mạc', index=1,
                                             options=['Yes', 'No'], horizontal=True)
-            eye_itching = st.radio(label='eye_itching', index=1,
+            eye_itching = st.radio(label='Ngứa mắt', index=1,
                                    options=['Yes', 'No'], horizontal=True)
-            eye_tearing = st.radio(label='eye_tearing', index=1,
+            eye_tearing = st.radio(label='Chảy nhiều nước mắt', index=1,
                                    options=['Yes', 'No'], horizontal=True)
     with cols1[1]:
         with st.expander(label='Urine', expanded=True):
-            bloody_urine = st.radio(label='bloody_urine', index=1,
+            bloody_urine = st.radio(label='Đi tiểu ra máu', index=1,
                                     options=['Yes', 'No'], horizontal=True)
-            pain_while_urinating = st.radio(label='pain_while_urinating', index=1,
+            pain_while_urinating = st.radio(label='Buốt khi tiểu', index=1,
                                             options=['Yes', 'No'], horizontal=True)
-            frequent_urination = st.radio(label='frequent_urination', index=1,
+            frequent_urination = st.radio(label='Tiểu nhiều', index=1,
                                           options=['Yes', 'No'], horizontal=True)
     cols2 = st.columns(2)
     with cols2[0]:
         with st.expander(label='Ear', expanded=True):
-            ear_pus = st.radio(label='ear_pus', index=1,
+            ear_pus = st.radio(label='Chảy mủ trong tai', index=1,
                                options=['Yes', 'No'], horizontal=True)
-            ear_pain = st.radio(label='ear_pain', index=1,
+            ear_pain = st.radio(label='Đau tai', index=1,
                                 options=['Yes', 'No'], horizontal=True)
-            ear_itching = st.radio(label='ear_itching', index=1,
+            ear_itching = st.radio(label='Ngứa tai', index=1,
                                    options=['Yes', 'No'], horizontal=True)
-            temporary_loss_hearing = st.radio(label='temporary_loss_hearing', index=1,
+            temporary_loss_hearing = st.radio(label='Mất thích giác tạm thời', index=1,
                                               options=['Yes', 'No'], horizontal=True)
     with cols2[1]:
         with st.expander(label='Skin', expanded=True):
-            infection_skin = st.radio(label='infection_skin', index=1,
+            infection_skin = st.radio(label='Mủ rộp trên da', index=1,
                                             options=['Yes', 'No'], horizontal=True)
-            skin_redness = st.radio(label='skin_redness', index=1,
+            skin_redness = st.radio(label='Da mẩn đỏ', index=1,
                                     options=['Yes', 'No'], horizontal=True)
-            skin_itching = st.radio(label='skin_itching', index=1,
+            skin_itching = st.radio(label='Ngứa da', index=1,
                                     options=['Yes', 'No'], horizontal=True)
-            pus_blisters_in_skin = st.radio(label='pus_blisters_in_skin', index=1,
+            pus_blisters_in_skin = st.radio(label='Bọng nước trên da', index=1,
                                             options=['Yes', 'No'], horizontal=True)
 
     with st.expander(label='Stomach', expanded=True):
-        bloody_waste = st.radio(label='bloody_waste', index=1,
+        bloody_waste = st.radio(label='Đi ngoài ra máu', index=1,
                                 options=['Yes', 'No'], horizontal=True)
-        stomach_cramps = st.radio(label='stomach_cramps', index=1,
+        stomach_cramps = st.radio(label='Chướng bụng', index=1,
                                   options=['Yes', 'No'], horizontal=True)
     submited = st.form_submit_button(label='Submit')
     if submited:
